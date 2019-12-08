22f817406f23b5cf88a5fe99d66d8405865256a6
Sean Carter <seans206email@gmail.com>
2019-12-08 12:29:18 -0800

    run time error
    run time error

---
+++ LetterValue.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+namespace AlphabetValues
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            ///*
+            //letter listed, @ provides 0 index value
+            List<char> alphabet = new List<char>() {'a', 'b', 'c', 'd', 'e', 'f',
+                'g','h','i','j','k','l','m','n','o','p','q','r','s',
+                't','u','v','w','x','y','z' };
+
+            //list of numbers created to create letter values
+            List<int> value = new List<int>();
+            for (int i = 1; i <= alphabet.Count; i++)
+                value.Add(i);
+               
+            for (var i = 0; i <= alphabet.Count; i++)
+                for (var j = 0; j <= alphabet.Count; j++)
+                    Console.WriteLine(alphabet[i] + ":" + value[j]);
