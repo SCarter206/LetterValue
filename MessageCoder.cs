a9174c6e4f5ccecb7747677986325a564dca88da
Sean Carter <seans206email@gmail.com>
2019-12-10 15:48:57 -0800

    Update MessageCoder.cs

--- MessageCoder.cs
+++ MessageCoder.cs
@@ -6,45 +6,23 @@ namespace AlphabetValues
 {
     class MessageCoder
     {
-        //letter listed, @ provides 0 index value
-        List<char> alphabet = new List<char>() {'a', 'b', 'c', 'd', 'e', 'f',
-                'g','h','i','j','k','l','m','n','o','p','q','r','s',
-                't','u','v','w','x','y','z' };
-
-
-        public void CodedMessage()
+        public void CodedMessage(Dictionary<char, int> letterAndValuesList, string userMessage)
         {
-            //list of numbers created to create letter values
-            List<int> value = new List<int>();
-            for (int i = 1; i <= alphabet.Count; i++)
+            List<int> numberList = new List<int>();
+            foreach(char letter in userMessage)
             {
-                value.Add(i);
-            }
-            
-            Console.WriteLine("input message here: ");
-            string x = Console.ReadLine();
-            //string codeMessage;
-            try
-            {
-                foreach (char letter in x)
+                foreach(KeyValuePair<char, int> letters in letterAndValuesList)
                 {
-                    foreach (char alphaletter in alphabet)
+                    if (letter == letters.Key)
                     {
-                        if (letter == alphaletter)
-                            Console.WriteLine(value[alphaletter]);
-                    }
-
+                        numberList.Add(letters.Value);
+                    }                   
                 }
             }
-            catch (ArgumentOutOfRangeException)
-            {
-                Console.WriteLine("we have an issue");
-                throw;
-            }
-            
-                
-        }
 
+            string numbers = String.Join("", numberList);
+            Console.WriteLine(numbers);
+        }
        
     }
 }
